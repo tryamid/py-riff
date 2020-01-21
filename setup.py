b976aa844bbef0b55a9b28269b29b8493e69f4e0
Tryamid <bsantanu381@gmail.com>
2020-01-21 08:35:40 +0530

    Fixed incompatible versioning from git describe

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ import subprocess
 
 setuptools.setup(
     name= 'py-riff',
-    version= subprocess.check_output(['git', 'describe', '--tags']).strip().decode('utf-8'),
+    version= subprocess.check_output(['git', 'describe', '--tags']).strip() \
+            .decode('utf-8') \
+            .split('-g')[0],
     description= "An encoding only implementation of the RIFF",
     author= "Shantanu Biswas",
     author_email= "bsantanu381@gmail.com",
