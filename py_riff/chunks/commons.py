d140b5305470abf60c88b9161fde0dc4e698aa2e
Shantanu <bsantanu381@gmail.com>
2020-01-21 08:02:26 +0530

    Addition of 'LIST', 'RIFX' chunks.

--- py_riff/chunks/commons.py
+++ py_riff/chunks/commons.py
@@ -2,12 +2,17 @@ from .base import NodeChunk, Chunk
 import typing
 
 class RIFF(NodeChunk):
-    """
-    RIFF is parent of all the chunks.
-    This is a vitual chunk, who's under all the chunks live.
-    """
     def __init__(self, fmt: typing.SupportsBytes):
         NodeChunk.__init__(self, b'RIFF')
 
         # put the format name before actual data.
         self.reservedbuf = bytes(fmt)
+
+class RIFX(NodeChunk):
+    def __init__(self, fmt: typing.SupportsBytes):
+        NodeChunk.__init__(self, b'RIFF', bigendian= True)
+        
+        self.reservedbuf = bytes(fmt)
+        
+class LIST(RIFF):
+    pass
