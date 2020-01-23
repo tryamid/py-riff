2e8ed43b9d66eac7a8e7e516a982483e1ac1aafa
Shantanu <bsantanu381@gmail.com>
2020-01-23 21:55:08 +0530

    Fixed ImportError and 'LIST' chunk acting as 'RIFF' chunk.
    LIST chunk inherited the RIFF chunk, which resulted 'LIST' to act the same like RIFF.
    Since, the base chunk code is moved to __init__.py the imports have to be fixed.

--- py_riff/chunks/commons.py
+++ py_riff/chunks/commons.py
@@ -1,4 +1,4 @@
-from .base import NodeChunk, Chunk
+from py_riff.__init__ import NodeChunk
 import typing
 
 class RIFF(NodeChunk):
@@ -14,5 +14,8 @@ class RIFX(NodeChunk):
         
         self.reservedbuf = bytes(fmt)
         
-class LIST(RIFF):
-    pass
+class LIST(NodeChunk):
+    def __init__(self, fmt: typing.SupportsBytes):
+        NodeChunk.__init__(self, b'LIST')
+        
+        self.reservedbuf = bytes(fmt)
